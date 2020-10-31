5e6b5aaf676abcf2824b4d4bd706bd1cf8cfbd10
JakeSumner11 <jakesumner@live.co.uk>
2020-10-31 12:28:21 +0000

    Testing dynamic parsing methods

--- public/javascripts/parser.js
+++ public/javascripts/parser.js
@@ -1,19 +1,45 @@
-
-
-function jsonParser(){
-JSON.parse()
-return getJson(parser);
-}
+//TODO: jquery event listener for when text entered into text area, when entered, getJson called,
+// data is updated onto new value and then sent to the parser that attempts to stringify it
+// and uses java api to put into correct glyphs.
 
 //get json data from textarea and continuously update when next text is entered
-function getJson(parser) {
 
+module.exports = {
 
-    let json = $('#jsonData').val();
+    textarea: document.getElementById('main-textarea'),
 
-    $('#jsonData').change(function () {
-        json = $(this).val();
-    });
+    newtextarea: document.getElementById('tester'),
+
+    getJSON: function () {
+        json = $('#main-textarea').val();
+
+        setJSON(json);
+    },
+
+    setJSON: function (jsonUpdate) {
+        $('#main-textarea').on('input propertychange paste', function () {
+            jsonUpdate = $(this).val();
+
+            $("#tester").val(jsonUpdate);
+        });
+    },
+
+    // parser: function () {
+    //
+    // }
 
 }
+
+
+
+var form = document.querySelector('form');
+form.addEventListener('change', function() {
+    alert('Hi!');
+});
+
+function updateResult() {
+    result.textContent = textarea.value;
+}
+
+textarea.onkeyup = updateResult;
 
