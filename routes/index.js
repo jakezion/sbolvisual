8285ee55d368cae7d31fe4689f1967859cc83424
JakeSumner11 <jakesumner@live.co.uk>
2020-11-24 22:12:56 +0000

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express'),
     JsonLdParser = require('jsonld-streaming-parser').JsonLdParser,
     jsonParser = new JsonLdParser(),
-    // jsonld = require('jsonld'),
+     jsonld = require('jsonld'),
     ParserJsonld = require('@rdfjs/parser-jsonld'),
     parserJsonld = new ParserJsonld(),
     Readable = require('stream').Readable,
     //fs = require('fs'),
     java = require('java'),
-    router = express.Router();
+    router = express.Router(),
+    detailed = false;
+
+let SBOL;
 
 
 router.use(express.urlencoded({extended: true}));
@@ -15,8 +18,12 @@ router.use(express.json());
 
 
 router.all('/', (req, res) => {
-    const sboldata = req.body.sboldata;
-    if (typeof sboldata !== 'undefined') parser(sboldata);
+    let sboldata = req.body.sboldata;
+
+    if (typeof sboldata !== 'undefined')  SBOL = parser(sboldata);
+
+
+
 
 
     // list(detailed);
@@ -31,8 +38,9 @@ router.all('/', (req, res) => {
         tagline: 'SBOL Visual is a web-based visualisation tool',
         keywords: ['SBOL', 'Visualisation', 'Synthetic Biology', 'SBOL v3', 'Glyph Creator'],
         copyright: 'Jake Sumner &copy; 2020',
-        sboldata: sboldata,
+        sboldata:  JSON.stringify(SBOL), //TODO: proper json stringify
         textarea: res.sendFile(__dirname + '/public/java/libSBOLj3/output/entity/collection/collection.jsonld') //fix
+
         //send array list with list data that then uses the built in loop system of ECTjs to parse the data into their own cards
     });
 
@@ -46,38 +54,30 @@ function parser(data) {
     // jsonParser.write(data);
     // jsonParser.end();
 
-        formatter(data);
+       // formatter(data);
+/*TODO: need to parse context in as well
+   add to array and call array whilst adding to context, reform json data and then send through formatter
+   if typeof context set context to this value
+   if typeof graph, for each send through parser with given context
+   console log to check */
+    let object = JSON.parse(data);
 
-
-    let testparse = JSON.parse(data);
-
-    for (let b in testparse) {
-        // console.log(b,"\n");
-        if (b === "@graph") {
-            for (let c in testparse[b]) {
-                //formatter(testparse[b][c]); //TODO: need to parse context in as well
-                //TODO: add to array and call array whilst adding to context, reform json data and then send through formatter
-            }
-
-        }
-        // else if (b === "@context"){
-        //     console.log(testparse[b]);
-        // }
-
-
-        //if typeof context set context to this value
-        //if typeof graph, for each send through parser with given context
-        //console log to check
-    }
-    for (let b in testparse) {
-        if (b === "@context") {
-        //    console.log(testparse[b]);
-        }
+    if (typeof object['@context'] === 'object') { //gets context
+    //    console.log(object['@context']);
     }
 
+    if (typeof object['@graph'] === 'object') { //gets context
+        return object['@graph'];
+       // return list(detailed,object['@graph']);
 
-    // console.log(dataArray);
+
+    }
+
+      // console.log(dataArray);
 }
+
+
+
 
 function formatter(data) {
     let dataArray = {};
@@ -90,15 +90,11 @@ function formatter(data) {
     });
     const output = parserJsonld.import(input);
 
-
-
     output.on('data', sbol => {
-        console.log(sbol);
+        console.log(sbol); //TODO: find way to group subject
 
         // setValue(sbol.predicate," : ",sbol.object);
         //  console.log(sbol.predicate," : ",sbol.object);
-
-
         dataArray[sbol.predicate.value] = sbol.object.value; //find correct grouping
 
        // list(false, dataArray);
@@ -112,8 +108,14 @@ function list(detailed, data) {
     if (!detailed) {
         // for (let attribute in data) {
         //     if (typeof attribute === "http://sbols.org/v3#displayId") {
-        //         let displayID = data[attribute];
-        //         setValue(displayID,"typeof");
+        // return new Promise((resolve, reject) => {
+        //     try {
+        //         //         let displayID = data[attribute];
+        //         //         setValue(displayID,"typeof");
+        //     } catch (err) {
+        //         reject(err)
+        //     }
+        // })
         //     }
         //     if (attribute === "http://sbols.org/v3#role") {
         //         let role = data[attribute];
