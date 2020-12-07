b8d83e2be62d70e806cc26c2906c62437184712f
JakeSumner11 <jakesumner@live.co.uk>
2020-12-07 20:48:06 +0000

    Attempt to set up java bridge

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 const express = require('express'),
-    JsonLdParser = require('jsonld-streaming-parser').JsonLdParser,
-    jsonParser = new JsonLdParser(),
-     jsonld = require('jsonld'),
     ParserJsonld = require('@rdfjs/parser-jsonld'),
-    parserJsonld = new ParserJsonld(),
     Readable = require('stream').Readable,
-    //fs = require('fs'),
-    java = require('java'),
+    //java = require('java'),
     router = express.Router(),
-    detailed = false;
+    API = require('../public/javascripts/API.java');
+    http = require("http");
+
+// detailed = false;
+// JsonLdParser = require('jsonld-streaming-parser').JsonLdParser,
+// jsonParser = new JsonLdParser(),
+// jsonld = require('jsonld'),
 
 let SBOL;
 
+/*TODO: setup maven for nodejs, then import the libSBOLj3 dependency then use node java to set up link */
 
 router.use(express.urlencoded({extended: true}));
 router.use(express.json());
@@ -20,13 +22,15 @@ router.use(express.json());
 router.all('/', (req, res) => {
     let sboldata = req.body.sboldata;
 
-    if (typeof sboldata !== 'undefined')  SBOL = parser(sboldata);
-
-
-
-
-
-    // list(detailed);
+    if (typeof sboldata !== 'undefined') SBOL = parser(sboldata)
+        // .then(parsed => console.log(parsed.co, parsed.gr))//test
+        //if setGlyph then formatter, setGlyph, otherwise just setList
+        // .then(data => formatter())
+        .then(data => setList(data.gr))
+        .catch(function (e) {
+            console.error(e.message);
+        })
+    // setList(detailed);
 
     res.setHeader('Cache-Control', 'no-cache');
 
@@ -38,54 +42,88 @@ router.all('/', (req, res) => {
         tagline: 'SBOL Visual is a web-based visualisation tool',
         keywords: ['SBOL', 'Visualisation', 'Synthetic Biology', 'SBOL v3', 'Glyph Creator'],
         copyright: 'Jake Sumner &copy; 2020',
-        sboldata:  JSON.stringify(SBOL,null, 4), //TODO: proper json stringify
+        sboldata: JSON.stringify(SBOL, null, 4), //TODO: proper json stringify
         textarea: res.sendFile(__dirname + '/public/java/libSBOLj3/output/entity/collection/collection.jsonld') //fix
 
-        //send array list with list data that then uses the built in loop system of ECTjs to parse the data into their own cards
+        //send array setList with setList data that then uses the built in loop system of ECTjs to parse the data into their own cards
     });
-
-
 });
 
+console.log(API);
 
-function parser(data) {
-    // jsonParser.on('context', console.log);
-    // jsonParser.on('data', console.log);
-    // jsonParser.write(data);
-    // jsonParser.end();
-
-       // formatter(data);
 /*TODO: need to parse context in as well
    add to array and call array whilst adding to context, reform json data and then send through formatter
    if typeof context set context to this value
    if typeof graph, for each send through parser with given context
    console log to check */
-    let object = JSON.parse(data);
 
-    if (typeof object['@context'] === 'object') { //gets context
-    //    console.log(object['@context']);
-        //Object.keys(object['@graph']).forEach((prefix) => {
-          //  this.emit('prefix', prefix, this.factory.namedNode(object['@context'][prefix])); //fix
-       // })
+const parser = async (sbol) => {
+
+    try {
+        let data = await getJSON(sbol);
+        let context = await getContext(data);
+        let graph = await getGraph(data);
+
+        return {co: context, gr: graph};
+
+
+    } catch (e) {
+        throw new Error(`Parsing Error`);
     }
-
-    if (typeof object['@graph'] === 'object') { //gets context
-
-       // return list(detailed,object['@graph']);
-
-        return object['@graph'];
-
-    }
-    // on('error', (err) => {
-    //     this.emit('error', err)
-    // })
-      // console.log(dataArray);
 }
 
 
+function getJSON(json) {
+    try {
+        return JSON.parse(json);
+    } catch (e) {
+        throw new Error(`Parsing Error`);
+    }
+
+}
+
+function getContext(object) {
+    if (typeof object['@context'] === 'object') { //gets context
+        return object['@context'];
+        //Object.keys(object['@graph']).forEach((prefix) => {
+        //  this.emit('prefix', prefix, this.factory.namedNode(object['@context'][prefix])); //fix
+        // })
+    }
+}
 
 
-function formatter(data) {
+function getGraph(object) {
+
+    if (typeof object['@graph'] === 'object') { //gets context
+        return object['@graph'];
+        //return object['@graph']; //promise wait for context then run formatter
+
+    }
+}
+
+function getGlyph(glyph, val) {
+    switch (val) {
+        case 'role':
+            console.log("Role glyph: ", glyph);
+            break;
+        case 'type':
+            console.log("Type glyph: ", glyph);
+            break;
+
+    }
+
+}
+
+function setValue(value, attribute) {
+    console.log(attribute, ": ", value);
+
+}
+
+
+function formatter(context, data) {
+    const parserJsonld = new ParserJsonld({
+        context: context
+    });
     let dataArray = {};
 
     const input = new Readable({
@@ -103,66 +141,45 @@ function formatter(data) {
         //  console.log(sbol.predicate," : ",sbol.object);
         dataArray[sbol.predicate.value] = sbol.object.value; //find correct grouping
 
-       // list(false, dataArray);
+        // setList(false, dataArray);
 
     });
 }
 
-function list(detailed, data) {
-    console.log(data, "\n\n");
+function setList(data) {
+    console.log('\n');
+    //  if (!detailed) {
+    for (let set in data) {
+        //gets each dataset
+        if (data[set].hasOwnProperty('displayId')) setValue(data[set]['displayId'], "DisplayID");
+        if (data[set].hasOwnProperty('description')) setValue(data[set]['description'], "Description");
+        if (data[set].hasOwnProperty('elements')) setValue(data[set]['elements'], "Elements");
 
-    if (!detailed) {
-        // for (let attribute in data) {
-        //     if (typeof attribute === "http://sbols.org/v3#displayId") {
-        // return new Promise((resolve, reject) => {
-        //     try {
-        //         //         let displayID = data[attribute];
-        //         //         setValue(displayID,"typeof");
-        //     } catch (err) {
-        //         reject(err)
-        //     }
-        // })
-        //     }
-        //     if (attribute === "http://sbols.org/v3#role") {
-        //         let role = data[attribute];
-        //         getGlyph(role);
-        //         setValue(role);
-        //     }
-        //     if (attribute === "http://sbols.org/v3#type") {
-        //         let type = data[attribute];
-        //         getGlyph(type);
-        //         setValue(type);
-        //     }
-        //     if (attribute === "http://sbols.org/v3#description") {
-        //         let description = data[attribute];
-        //         setValue(description);
-        //     }
-        //     if (attribute === "http://sbols.org/v3#elements") {
-        //         let elements = data[attribute];
-        //         setValue(elements);
-        //     }
-        // }
-
-    } else if (detailed) {
-        for (let attribute in data) {
-            if (attribute === "http://sbols.org/v3#elements") {
-                console.log(attribute, ":", data[attribute]);
-            }
+        if (data[set].hasOwnProperty('role')) {
+            setValue(data[set]['role'], "Role");
+            getGlyph(data[set]['role'], 'role');
         }
-        // Array.forEach((item) => {
-        //     let list = item.whole.value;
-        //
-        // })
+        if (data[set].hasOwnProperty('type')) {
+            setValue(data[set]['type'], "Type");
+            getGlyph(data[set]['type'], 'type');
+        }
+
+        console.log('\n');
     }
-}
 
-function getGlyph(glyph) {
-    console.log("glyph: ", glyph);
-}
-
-function setValue(value) {
-    console.log("Object", value);
-
+    // } else if (detailed) {
+    for (let set in data) {
+        try {
+            if (data[set].hasOwnProperty('role')) setValue(data[set]['role'], "Role");
+            if (data[set].hasOwnProperty('type')) setValue(data[set]['type'], "Type");
+        } catch (e) {
+            throw new Error(`parse err`);
+        } finally {
+            console.log(data[set]);
+        }
+    }
+    //     }
+    // }
 }
 
 
@@ -170,8 +187,8 @@ module.exports = router;
 
 /*
 POST recieved, data parsed to json-ld format
-data then sent through API to get correct glyph type
-and other details relating to the glyph for the list view
+data then sent through API.java to get correct glyph type
+and other details relating to the glyph for the setList view
 data sent back to parser where it will rendered in router request
 and displayed in the correct locations, using the view engine.
 Make repeatable
