742cc524d0849694999ab51d39d29f641f4cff5d
JakeSumner11 <jakesumner@live.co.uk>
2021-03-14 21:42:27 +0000

    Initial Commit

--- app.js
+++ app.js
@@ -49,11 +49,19 @@ app.use('/about', staticRouter);
 app.use('/links', staticRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send("Sorry can't find that!")
+    res.status(404).send("Sorry can't find that!");
+});
+
+app.use((req, res, next) => {
+    res.status(304).send("Not modified");
+});
+
+app.use((req, res, next) => {
+    res.status(200).send("Working as intended");
 });
 
 app.use( (req, res, next) => {
-    res.status(500).send('Something broke!')
+    res.status(500).send('Something broke!');
 });
 
 app.listen(app.port, () => {
